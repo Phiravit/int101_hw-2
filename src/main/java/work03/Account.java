e7013cbaa61407ab3d88828820f9013c5994d8c6
Phiravit Imsuchart <100277519+Phiravit@users.noreply.github.com>
2023-11-13 00:34:45 +0700

    Update Account.java

--- src/main/java/work03/Account.java
+++ src/main/java/work03/Account.java
@@ -1,101 +1,66 @@
 package work03;
-
 import work01.Utilitor;
 import work02.Person;
 
 public class Account {
-
     private static long nextNo = 100_000_000;
     private final long no;
-    private final Person owner;
+    private Person owner;
     private double balance;
 
-    public Account(Person owner) {
-        if (owner == null) {
-            throw new NullPointerException("Owner cannot be null.");
+    public Account(Person owner){
+        if(owner == null){
+            throw new NullPointerException();
         }
-
         this.owner = owner;
         this.balance = 0.0;
+        long result = 0;
 
-        long temporaryNo;
-
-        do {
-            int result;
-            do {
-                result = (int) Utilitor.computeIsbn10(nextNo);
-                if (result == 10) {
-                    nextNo++;
-                }
-            } while (result == 10);
-
-            temporaryNo = 10 * nextNo + result;
-            nextNo++;
-
-        } while (Utilitor.testPositive((double) temporaryNo) <= 0);
-
-        this.no = temporaryNo;
-
-    }
-
-    public long getNo() {
-        return no;
-    }
-
-    public Person getOwner() {
-        return owner;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public double deposit(double amount) {
-        if (Utilitor.testPositive(amount) <= 0) {
-            throw new IllegalArgumentException("Amount must be positive.");
+        while (true){
+            result = Utilitor.computeIsbn10(Account.nextNo);
+            if (result != 10){
+                break;
+            }
+            Account.nextNo += 1;
         }
-
-        this.balance += amount;
+        this.no = 10 * Account.nextNo + result;
+        Account.nextNo++;
+    }
+    public long getNo(){
+        return this.no;
+    }
+    public Person getOwner(){
+        return this.owner;
+    }
+    public double getBalance(){
         return this.balance;
     }
-
-    public double withdraw(double amount) {
-        if (Utilitor.testPositive(amount) <= 0) {
-            throw new IllegalArgumentException("Amount must be positive.");
+    public double deposit(double amount){
+        return this.balance += Utilitor.testPositive(amount);
+    }
+    public double withdraw(double amount){
+        if(amount > this.balance){
+            throw new IllegalArgumentException("Invalid amount");
         }
-
-        double newBalance = this.balance - amount;
-
-        if (Utilitor.testPositive(newBalance) <= 0) {
-            throw new IllegalArgumentException("Withdrawal would result in a non-positive balance.");
-        }
-
-        this.balance = newBalance;
+        this.balance -= Utilitor.testPositive(amount);
         return this.balance;
     }
-
-    public void transfer(double amount, Account account) {
-        if (account == null || account.withdraw(amount) <= 0) {
-            throw new IllegalArgumentException("Invalid transfer: target account is null or cannot withdraw the specified amount.");
+    public void transfer(double amount, Account account){
+        if (account == null || amount > this.balance || amount < 0){
+            throw new IllegalArgumentException("Invalid Input");
         }
-
-        this.deposit(amount);
+        this.withdraw(amount);
+        account.deposit(amount);
     }
-
     @Override
-    public String toString() {
-        return "Account(" + no + "," + balance + ")";
+    public String toString(){
+        return "Account {No = " + this.no + ", balamce = "+ this.balance +" }";
     }
-
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object obj){
+        if (this == obj){
             return true;
         }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Account account = (Account) obj;
-        return this == account;
+        return false;
     }
 }

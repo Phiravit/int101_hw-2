19cbec2dd58d381edd6e04f57b55c8771a4509ea
Phiravit Imsuchart <100277519+Phiravit@users.noreply.github.com>
2023-11-13 00:34:01 +0700

    updated fix corrupted files

--- src/main/java/com/mycompany/homework02/homework02.java
+++ src/main/java/com/mycompany/homework02/homework02.java
@@ -53,65 +53,10 @@ public class homework02 {
         System.out.println("person1 = person2? " + person1.equals(person2));
     }
 
-    static void work03Account() {package com.mycompany.homework02;
-
-import work01.Utilitor;
-import work02.Person;
-import work03.Account;
-
-public class homework02 {
-
-    public static void main(String[] args) {
-        work01Utilitor();
-        work02Person();
-        work03Account();
-    }
-
-    static void work01Utilitor() {
-        Utilitor utilitor = new Utilitor();
-        try {
-            String stringValue = utilitor.testString("Hello");
-            System.out.println("testString : " + stringValue);
-        } catch (NullPointerException | IllegalArgumentException e) {
-            System.err.println("Error : " + e.getMessage());
-        }
-
-        try {
-            double positiveValue = utilitor.testPositive(5.0);
-            System.out.println("testPositive : " + positiveValue);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Error : " + e.getMessage());
-        }
-
-        try {
-            long isbn10 = 213856789L;
-            long checkDigit = utilitor.computeIsbn10(isbn10);
-            System.out.println("computeIsbn10 : " + checkDigit);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Error : " + e.getMessage());
-        }
-    }
-
-    static void work02Person() {
-        Person person1 = new Person("John", "Doe");
-        Person person2 = new Person("Joe", "Dane");
-
-        System.out.println("Before modification:");
-        System.out.println(person1);
-
-        person1.setFirstname("Johhpy");
-        person1.setLastname("Deppy");
-
-        System.out.println("After modification:");
-        System.out.println(person1);
-
-        System.out.println("person1 = person2? " + person1.equals(person2));
-    }
-
     static void work03Account() {
         Person person1 = new Person("Phiravit", "GogetAjob");
-        Person person2 = new Person("Spong","Wet");
-        Person person3 = new Person("Funky","Japanesegoblin");
+        Person person2 = new Person("spong","wet");
+        Person person3 = new Person("funky","japanesegoblin");
         Account account1 = new Account(person1);
         Account account2 = new Account(person2);
         Account account3 = new Account(person3);
@@ -124,16 +69,13 @@ public class homework02 {
         System.out.println(account1.getBalance());
         System.out.println(account1.getOwner());
 
-        account1.deposit(254564);
+        account1.deposit(2564);
         System.out.println(account1);
 
-        account1.transfer(41231,account3);
+        account1.transfer(1231,account3);
         System.out.println(account1);
         System.out.println(account3);
 
         System.out.println(account1.equals(account2));
-    }
-}
-
     }
 }
